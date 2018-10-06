3de9bdb2312e6a881179fb991499fbf175dc93e0
smhongok <smhongok@gmail.com>
2018-10-06 19:16:16 +0900

    PID.cpp

--- include/project1/pid.cpp
+++ include/project1/pid.cpp
@@ -8,13 +8,25 @@ PID::PID(){
      *
     */
 
+	Kp=1.0;
+	Ki=1.0;
+	Kd=1.0;
+	error = 0.0;
+	error_sum = 0.0;
+	error_diff = 0.0;
+	
+
 }
 
 float PID::get_control(point car_pose, point goal_pose){
 
     float ctrl;
+	error_sum += error;      
+	error_diff =car_pose.th - goal_pose.th - error;
+        error = car_pose.th - goal_pose.th;
 
-    /* TO DO
+	ctrl = Kp* error + Ki*error_sum + Kd*error_diff;   
+ /* TO DO
      *
      * implement pid algorithm
      *
